32d6454e4f4df75680f14000bc659dd95d361420
Deekshith Rathod <68671324+Deekshithrathod@users.noreply.github.com>
2026-05-30 14:08:50 +0530

    Configure blog custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://rasaecology.com',
+  site: 'https://blog.rasaecology.com',
   integrations: [sitemap()],
   output: 'static',
 });
